6b33a15933d83c522efe598b09f48a3620cd73be
Zach3292 <zach3292@hotmail.com>
2022-08-01 10:21:50 -0400

    potentially working full speed steering

--- custom_algorythm.py
+++ custom_algorythm.py
@@ -1,8 +1,11 @@
+from ast import If
 from tmrl import get_environment
 from time import sleep
 from os import system, name
 import numpy as np
 import random
+
+from torch import round_
 
 def clear(): 
   
@@ -20,7 +23,9 @@ env = get_environment()  # retrieve the TMRL Gym environment
 
 # for now we'll only try to make the car steer so 3 simple state with lidar to see if the road is straight ahead, right or left and 3 simple action for straight, right and left
 q_table = np.zeros([3, 3])
-
+# 0 = left
+# 1 = nothing
+# 2 = right
 
 training_episodes = 20000 # Amount of times to run environment while training.
 display_episodes = 10 # Amount of times to run environment after training.
@@ -34,6 +39,25 @@ epsilon = 0.1 # Chance of selecting a random action instead of maximising reward
 all_epochs = []
 all_penalties = []
 
+
+
+# default observations are of shape: ((1,), (4, 19), (3,), (3,))
+# representing: (speed, 4 last LIDARs, 2 previous actions)
+# actions are [gas, break, steer], analog between -1.0 and +1.0
+
+
+def obsToState(obs):
+    deviation = obs[1].mean(0)
+    deviation /= (deviation.sum() + 0.001)
+    steer = 0
+    for i in range(19):
+        steer += (i - 9) * deviation[i]
+    steer = - np.tanh(steer * 4)
+    steer = min(max(steer, -1.0), 1.0)
+
+    state = round(steer) + 1  # To transform the deviation into the 3 state of the array
+    return state
+
 """Training the Agent"""
 
 sleep(1.0)
@@ -44,24 +68,31 @@ for i in range(training_episodes):
     penalties, rew, = 0, 0
     
     while not term:
+
+
+        state = obsToState(obs)
+
         if random.uniform(0, 1) < epsilon:
             action = np.array([1.0, 0, random.uniform([-1, 1])]) # Pick a new action for this state.
         else:
-            action = np.argmax(q_table[obs]) # Pick the action which has previously given the highest reward.
-
-        obs, rew, term, trun, info = env.step(action) 
+            action = np.array([1.0, 0, (np.argmax(q_table[state])) - 1]) # Pick the action which has previously given the highest reward.
         
-        old_value = q_table[obs, action] # Retrieve old value from the q-table.
+
+        next_obs, rew, term, trun, info = env.step(action) 
+
+        next_state = obsToState(next_obs)
+        
+        old_value = q_table[state, action] # Retrieve old value from the q-table.
         next_max = np.max(q_table[next_state])
 
         # Update q-value for current state.
         new_value = (1 - alpha) * old_value + alpha * (rew + gamma * next_max)
-        q_table[obs, action] = new_value
+        q_table[state, action] = new_value
 
         if rew == -10: # Checks if agent attempted to do an illegal action.
             penalties += 1
 
-        obs = next_state
+        state = next_state
         
     if i % 100 == 0: # Output number of completed episodes every 100 episodes.
         print(f"Episode: {i}")
@@ -78,7 +109,10 @@ for _ in range(display_episodes):
     term = False
     
     while not term:
-        action = np.argmax(q_table[obs])
+
+        state = obsToState(obs)
+
+        action = np.argmax(q_table[state])
         obs, rew, term, trun, info = env.step(action) 
 
         if rew == -10:
@@ -88,7 +122,7 @@ for _ in range(display_episodes):
         clear()
         env.render()
         print(f"Timestep: {epochs}")
-        print(f"State: {obs}")
+        print(f"State: {state}")
         print(f"Action: {action}")
         print(f"Reward: {rew}")
         sleep(0.15) # Sleep so the user can see the 
@@ -102,32 +136,3 @@ print(f"Average penalties per episode: {total_penalties / display_episodes}")
 
 
 
-"""
-# default observations are of shape: ((1,), (4, 19), (3,), (3,))
-# representing: (speed, 4 last LIDARs, 2 previous actions)
-# actions are [gas, break, steer], analog between -1.0 and +1.0
-def model(obs):
-    
-    # simplistic policy
-    
-    deviation = obs[1].mean(0)
-    deviation /= (deviation.sum() + 0.001)
-    steer = 0
-    for i in range(19):
-        steer += (i - 9) * deviation[i]
-    steer = - np.tanh(steer * 4)
-    steer = min(max(steer, -1.0), 1.0)
-    return np.array([1.0, 0.0, steer])
-
-env = get_environment()  # retrieve the TMRL Gym environment
-
-sleep(1.0)  # just so we have time to focus the TM20 window after starting the script
-
-obs = env.reset()  # reset environment
-for _ in range(200):  # rtgym ensures this runs at 20Hz by default
-    act = model(obs)  # compute action
-    obs, rew, term, trun, info = env.step(act)  # apply action (rtgym ensures healthy time-steps)
-    if term:
-        break
-env.wait()  # rtgym-specific method to artificially 'pause' the environment when needed
-"""

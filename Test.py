e0580adee437d845d1526e54bbda00ee5d631237
Zach3292 <zach3292@hotmail.com>
2022-10-21 19:06:34 -0400

    upload to github

--- Test.py
+++ Test.py
@@ -4,18 +4,7 @@ import numpy as np
 data_path = "C:\\Users\\Zach\\Documents\\001 TMRL\\nparray"
 created_folder = "C:\\Users\\Zach\\Documents\\001 TMRL\\nparray"
 
-path = os.path.join(data_path, created_folder, 'testone')
-with open('{}.npy'.format(path), 'wb') as f:
-     np.save(f, np.arange(10))
-
-array = np.load("C:\\Users\\Zach\\Documents\\001 TMRL\\nparray\\testone.npy")
+array = np.load("C:\\Users\\Zach\\Documents\\001 TMRL\\nparray\\neuralNetwork.npy")
 
 print(array)
 
-path = os.path.join(data_path, created_folder, 'testone')
-with open('{}.npy'.format(path), 'wb') as f:
-     np.save(f, np.arange(5))
-
-array = np.load("C:\\Users\\Zach\\Documents\\001 TMRL\\nparray\\testone.npy")
-
-print(array)
